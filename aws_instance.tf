5a2cf3bf7ff03790e132594fbb7ee5985b481007
Yuki Uchiyama <uchiyama.yuki@moneyforward.co.jp>
2022-05-16 14:53:30 +0900

    Add security group and instance tf

---
+++ aws_instance.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "cm-test" {
+    ami = "ami-cbf90ecb"
+    instance_type = "t2.micro"
+    key_name = "yuki.mf"
+    vpc_security_group_ids = [
+      "${aws_security_group.adminTF.id}"
+    ]
+    subnet_id = "${aws_subnet.tf-public-a.id}"
+    associate_public_ip_address = "true"
+    root_block_device = {
+      volume_type = "gp2"
+      volume_size = "20"
+    }
+    ebs_block_device = {
+      device_name = "/dev/sdf"
+      volume_type = "gp2"
+      volume_size = "100"
+    }
+    tags {
+        Name = "cm-test"
+    }
+}
---
+++ aws_security_group.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "adminTF" {
+    name = "adminTf"
+    description = "Allow SSH inbound traffic"
+    vpc_id = "${aws_vpc.myTfVPC.id}"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
